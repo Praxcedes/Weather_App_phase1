b07bec77d782a4df21e4c455f8ce3d1a6dd02de2
Praxcedes <praxcedeskabeya@gmail.com>
2025-07-22 15:46:05 -0700

    add commit

---
+++ style.js
@@ -0,0 +1,7 @@
+// DOM Elements
+const cityInput = document.getElementById('cityInput');
+const searchBtn = document.getElementById('searchBtn');
+const weatherInfo = document.getElementById('weatherInfo');
+const suggestionsContainer = document.getElementById('suggestions');
+const themeToggle = document.getElementById('themeToggle');
+
